c96619e49641fbe3bd3df37538bd927a27d2a782
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-06-30 09:13:30 +0530

    added HomeScreen render test

---
+++ __tests__/HomeScreen-test.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import HomeScreen from '../src/screens/HomeScreen/HomeScreen';
+import Strings from '../src/i18n/en';
+import { loggedInUserType } from '../src/context/type';
+import { AuthContext } from '../src/context/AuthContext';
+
+const loggedInUserDataDummy = {
+  id: '123abc',
+  name: 'Jane Doe',
+  profileUrl: 'https://via.placeholder.com/600/d32776',
+};
+
+function renderHomeScreen({
+  loggedInUserData,
+}: {
+  loggedInUserData: loggedInUserType | null;
+}) {
+  const isLoading = false;
+  const setIsLoading = () => {};
+  const setLoggedInUserData = () => {};
+
+  return render(
+    <AuthContext.Provider
+      value={{
+        isLoading,
+        setIsLoading,
+        loggedInUserData,
+        setLoggedInUserData,
+      }}
+    >
+      <HomeScreen />
+    </AuthContext.Provider>,
+  );
+}
+
+describe('renders correctly', () => {
+  test('renders I am OOO when loggedInUserData !null & status = OOO', () => {
+    const { getByText } = renderHomeScreen({
+      loggedInUserData: {
+        ...loggedInUserDataDummy,
+        status: Strings.OUT_OF_OFFICE,
+      },
+    });
+    getByText(Strings.OOOStatus_Text);
+  });
+  test('renders I am Idle when loggedInUserData !null & status = Idle', () => {
+    const { getByText } = renderHomeScreen({
+      loggedInUserData: {
+        ...loggedInUserDataDummy,
+        status: Strings.IDLE,
+      },
+    });
+    getByText(Strings.Idle_Text);
+  });
+  test('renders I am doing a task when loggedInUserData !null & status = Active', () => {
+    const { getByText } = renderHomeScreen({
+      loggedInUserData: {
+        ...loggedInUserDataDummy,
+        status: Strings.ACTIVE,
+      },
+    });
+    getByText(Strings.Active_Text);
+  });
+});
+
+describe('changeStatus', () => {
+  test.todo('changeStatus OOO, makes loggedInUserData.status OOO');
+  test.todo('changeStatus Idle, makes loggedInUserData.status Idle');
+  test.todo('changeStatus Active, makes loggedInUserData.status Active');
+});
+
+describe('changeStatus operations', () => {
+  test.todo('if status idle, change status to active');
+  test.todo('if status active, change status to idle');
+  test.todo('if status !OOO, mark yourself as OOO');
+  test.todo('if status OOO, mark yourself active');
+});
