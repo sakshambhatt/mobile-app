bea35e82d93776be6173810f7a4afdfa3499de76
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-07-01 09:02:28 +0530

    added storeData test

--- __tests__/HomeScreen-test.tsx
+++ __tests__/HomeScreen-test.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import HomeScreen from '../src/screens/HomeScreen/HomeScreen';
 import Strings from '../src/i18n/en';
 import { loggedInUserType } from '../src/context/type';
 import { AuthContext } from '../src/context/AuthContext';
+import { storeData } from '../src/hooks/dataStoreHook';
 
 const loggedInUserDataDummy = {
   id: '123abc',
   name: 'Jane Doe',
   profileUrl: 'https://via.placeholder.com/600/d32776',
+  status: 'ooo',
 };
 
 function renderHomeScreen({
-  loggedInUserData,
+  initialLoggedInUserData,
 }: {
-  loggedInUserData: loggedInUserType | null;
+  initialLoggedInUserData: loggedInUserType | null;
 }) {
   const isLoading = false;
   const setIsLoading = () => {};
-  const setLoggedInUserData = () => {};
+
+  let loggedInUserData = initialLoggedInUserData;
+  const setLoggedInUserData = (userData: loggedInUserType | null) => {
+    loggedInUserData = userData;
+  };
 
   return render(
     <AuthContext.Provider
@@ -37,7 +44,7 @@ function renderHomeScreen({
 describe('renders correctly', () => {
   test('renders I am OOO when loggedInUserData !null & status = OOO', () => {
     const { getByText } = renderHomeScreen({
-      loggedInUserData: {
+      initialLoggedInUserData: {
         ...loggedInUserDataDummy,
         status: Strings.OUT_OF_OFFICE,
       },
@@ -46,7 +53,7 @@ describe('renders correctly', () => {
   });
   test('renders I am Idle when loggedInUserData !null & status = Idle', () => {
     const { getByText } = renderHomeScreen({
-      loggedInUserData: {
+      initialLoggedInUserData: {
         ...loggedInUserDataDummy,
         status: Strings.IDLE,
       },
@@ -55,7 +62,7 @@ describe('renders correctly', () => {
   });
   test('renders I am doing a task when loggedInUserData !null & status = Active', () => {
     const { getByText } = renderHomeScreen({
-      loggedInUserData: {
+      initialLoggedInUserData: {
         ...loggedInUserDataDummy,
         status: Strings.ACTIVE,
       },
@@ -64,10 +71,28 @@ describe('renders correctly', () => {
   });
 });
 
+describe('storeData', () => {
+  test('storeData name, value stores in AsyncStorage', async () => {
+    await storeData('userData', JSON.stringify(loggedInUserDataDummy));
+    expect(AsyncStorage.setItem).toBeCalledWith(
+      'userData',
+      JSON.stringify(loggedInUserDataDummy),
+    );
+    const res = await AsyncStorage.getItem('userData');
+    expect(res).toMatch(JSON.stringify(loggedInUserDataDummy));
+  });
+});
+
 describe('changeStatus', () => {
-  test.todo('changeStatus OOO, makes loggedInUserData.status OOO');
-  test.todo('changeStatus Idle, makes loggedInUserData.status Idle');
-  test.todo('changeStatus Active, makes loggedInUserData.status Active');
+  test.todo(
+    'when status != OOO, changeStatus OOO, makes loggedInUserData.status OOO',
+  );
+  test.todo(
+    'when status != Idle, changeStatus Idle, makes loggedInUserData.status Idle',
+  );
+  test.todo(
+    'when status !=Active, changeStatus Active, makes loggedInUserData.status Active',
+  );
 });
 
 describe('changeStatus operations', () => {
