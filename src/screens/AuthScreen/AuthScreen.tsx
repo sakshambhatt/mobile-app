dd1669e8d7afb6d2bd0085d7d9903eca6d79e8d6
Saksham Bhatt <sakshambhatt33@gmail.com>
2023-04-10 21:37:53 +0530

    refactor: removed GithuView & added logic for toast in handleSignIn

--- src/screens/AuthScreen/AuthScreen.tsx
+++ src/screens/AuthScreen/AuthScreen.tsx
@@ -1,32 +1,19 @@
-import React, { useContext, useState } from 'react';
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  ActivityIndicator,
-  SafeAreaView,
-} from 'react-native';
-import WebView from 'react-native-webview';
-import { urls } from '../../constants/appConstant/url';
-import { AuthContext } from '../../context/AuthContext';
-import Images from '../../constants/images/Image';
-import { storeData } from '../../utils/dataStore';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import Strings from '../../i18n/en';
 import { AuthViewStyle } from './styles';
 import { AuthScreenButton } from './Button';
 import { OtpModal } from './OtpModal';
-import { getUserData } from './Util';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 const AuthScreen = () => {
-  const { setLoggedInUserData } = useContext(AuthContext);
-  const [githubView, setGithubView] = useState<boolean>(false);
+  // const { setLoggedInUserData } = useContext(AuthContext);
+  // const [githubView, setGithubView] = useState<boolean>(false);
   const [otpCode, setOtpCode] = useState<string>('');
   const [otpModalVisible, setOtpModalVisible] = useState<boolean>(false);
-  const [addressbarURL, setAdressbarURL] = useState<String>('');
-  const [loading, setLoading] = useState(false);
-  const [key, setKey] = useState(1);
+  // const [addressbarURL, setAdressbarURL] = useState<String>('');
+  // const [loading, setLoading] = useState(false);
+  // const [key, setKey] = useState(1);
 
   const closeModal = () => {
     setOtpModalVisible(false);
@@ -38,7 +25,13 @@ const AuthScreen = () => {
   //TODO: add to constants
   const maxLength = 4;
   const handleSignIn = () => {
-    setGithubView(true);
+    // NOTE: toast until sign in with Github is implemented
+    Toast.show({
+      type: 'info',
+      text1: 'Sign in with GitHub coming soon...',
+      position: 'bottom',
+      bottomOffset: 80,
+    });
   };
 
   const updateUserData = async (url: string) => {
